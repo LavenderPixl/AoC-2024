a6c7d24a2401785198c411e82aafe362b22b81d9
LavenderPixl <annajacobsen1999@hotmail.com>
2024-12-12 08:53:58 +0100

    Day 3, Part 1 Done in Python

---
+++ AoC/Day3/Python/day3.py
@@ -0,0 +1,17 @@
+import re
+
+reports = open("AoC/Day3/input.txt", "r").read().splitlines()
+pattern = re.compile(r'mul\(([0-9]*),([0-9]*)\)')
+
+
+def part_one():
+    matches = []
+    result = 0
+    for report in reports:
+        for (a, b) in re.findall(pattern, report):
+            matches.append(int(a) * int(b))
+
+    for r in matches:
+        result += r
+
+    print(result)
