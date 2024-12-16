2c90aaf084b9b9ca51481c68727cd3be89f4d358
LavenderPixl <annajacobsen1999@hotmail.com>
2024-12-16 13:56:41 +0100

    Day 3, Part 1 Done in C#.

---
+++ AoC/Day3/CSharp/Day3Part1.cs
@@ -0,0 +1,45 @@
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace AoC.Day3.CSharp;
+
+public class Day3Part1 : IDay
+{
+    private string _input = "";
+    private const string Pattern = @"mul\(([0-9]*),([0-9]*)\)";
+    private readonly List<int> _inputList = [];
+    
+    public void Run()
+    {
+        HandleInput("../../../Day3/input.txt");
+        FindResults();
+    }
+
+    public void HandleInput(string path)
+    {
+        _input = File.ReadAllText(path);
+    }
+
+    private void FindResults()
+    {
+        var res = 0;
+
+        try
+        {
+            var matches = Regex.Matches(_input, Pattern);
+            foreach (Match match in matches)
+            {
+                _inputList.Add((int.Parse(match.Groups[1].Value)) * (int.Parse(match.Groups[2].Value)));
+            }
+
+            res += _inputList.Sum();
+
+            Console.WriteLine(res);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Woops - No Input file? Err: {e}");
+            throw;
+        }
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,15 +1,25 @@
-﻿namespace AoC;
-using AoC.Day1.CSharp;
+﻿using AoC.Day1.CSharp;
+using AoC.Day2.CSharp;
+using AoC.Day3.CSharp;
+
+namespace AoC;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Day1.CSharp.Day1Part1 day1Part1 = new();
+        //Day 1
+        Day1Part1 day1Part1 = new();
         day1Part1.Run();
-        Day1.CSharp.Day1Part2 day1Part2 = new();
+        Day1Part2 day1Part2 = new();
         day1Part2.Run();
-        
-        // Day2.CSharp.Day2Part1 day2Part1 = new(File.ReadAllText("../../../Day2/input.txt").Split("\n"));
+
+        //Day 2
+        Day2Part1 day2Part1 = new();
+        day2Part1.Run();
+
+        //Day 3
+        Day3Part1 day3Part1 = new();
+        day3Part1.Run();
     }
 }
