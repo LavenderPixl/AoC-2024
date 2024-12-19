d481b809185fe2a3b36dd0a949a4f65bb77a694a
LavenderPixl <annajacobsen1999@hotmail.com>
2024-12-19 13:16:53 +0100

    C# user input now matters too! (woop)

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Day1.CSharp;
+﻿using System.Threading.Channels;
+using AoC.Day1.CSharp;
 using AoC.Day2.CSharp;
 using AoC.Day3.CSharp;
 
@@ -8,18 +9,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Day 1
-        Day1Part1 day1Part1 = new();
-        day1Part1.Run();
-        Day1Part2 day1Part2 = new();
-        day1Part2.Run();
+        bool redo;
+        do
+        {
+            RunDay();
+            redo = ReDo();
+        } while (redo);
 
-        //Day 2
-        Day2Part1 day2Part1 = new();
-        day2Part1.Run();
+        void RunDay()
+        {
+            Console.Clear();
+            Console.WriteLine("Which day would you like to see? \n- 1-3 for a specific day\n- * for all days.");
+            var reply = Console.ReadLine();
 
-        //Day 3
-        Day3Part1 day3Part1 = new();
-        day3Part1.Run();
+            Day1Part1 day1Part1 = new();
+            Day1Part2 day1Part2 = new();
+            Day2Part1 day2Part1 = new();
+            Day3Part1 day3Part1 = new();
+            switch (reply)
+            {
+                case "1":
+                    //Day 1
+                    day1Part1.Run();
+                    day1Part2.Run();
+                    break;
+                case "2":
+                    //Day 2
+                    day2Part1.Run();
+                    break;
+                case "3":
+                    //Day 3
+                    day3Part1.Run();
+                    break;
+                case "*":
+                    day1Part1.Run();
+                    day1Part2.Run();
+                    day2Part1.Run();
+                    day3Part1.Run();
+                    break;
+                default:
+                    Console.WriteLine("That's not a valid input.");
+                    break;
+            }
+        }
+
+        bool ReDo()
+        {
+            bool userInput;
+            Console.WriteLine("Would you like to retry again? Y/N");
+            var reply = Console.ReadLine();
+            reply = reply?.ToLower();
+
+            switch (reply)
+            {
+                case "y":
+                    userInput = true;
+                    break;
+                case "n" :
+                    userInput = false;
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Input.");
+                    userInput = true;
+                    break;
+            }
+
+            return userInput;
+        }
     }
 }
