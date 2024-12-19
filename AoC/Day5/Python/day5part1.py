68b0a851a4acc4c0a246621586bfb2635f6b5bc7
LavenderPixl <annajacobsen1999@hotmail.com>
2024-12-19 10:32:44 +0100

    Day 5, Part 1 Done in Python.

---
+++ AoC/Day5/Python/day5part1.py
@@ -0,0 +1,52 @@
+def run():
+    rules, updates = clean_data()
+    print(do_stuff(rules, updates))
+
+
+def clean_data():
+    cleaned_rules = []
+    cleaned_updates = []
+    data = open("AoC/Day5/input.txt", "r").read().split("\n\n")
+    rules, updates = data[0], data[1]
+    rules = rules.splitlines()
+    updates = updates.splitlines()
+    for rule in rules:
+        left, right = rule.rsplit("|", maxsplit=1)
+        cleaned_rules.append((left, right))
+    for update in updates:
+        re = update.rsplit(",")
+        cleaned_updates.append(re)
+
+    return cleaned_rules, cleaned_updates
+
+
+def check_rules(rules, updates):
+    result = []
+    for update in updates:
+        correct = True
+        for i in range(len(update)):
+            for j in range(i + 1, len(update)):
+                for rule in rules:
+                    if update[i] in rule and update[j] in rule:
+                        if update[i] == rule[0] and update[j] == rule[1]:
+                            correct = True
+                        else:
+                            correct = False
+                            break
+                    if not correct:
+                        break
+                if not correct:
+                    break
+        if correct:
+            result.append(update)
+
+    return result
+
+def do_stuff(rules, updates):
+    returned_list = check_rules(rules, updates)
+    result = 0
+    for i_list in returned_list:
+        middle_index = (len(i_list) - 1) // 2
+        res = i_list[middle_index]
+        result += int(res)
+    return result
--- AoC/Main.py
+++ AoC/Main.py
@@ -2,10 +2,17 @@ from AoC.Day1.Python.day1part1 import run as day1part1
 from AoC.Day1.Python.day1part2 import run as day1part2
 from AoC.Day2.Python.day2part1 import run as day2part1
 from AoC.Day3.Python.day3part1 import run as day3part1
+from AoC.Day4.Python.day4part1 import run as day4part1
+from AoC.Day5.Python.day5part1 import run as day5part1
 
-day1part1()
-day1part2()
 
-day2part1()
+# day1part1()
+# day1part2()
+#
+# day2part1()
+#
+# day3part1()
+#
+# day4part1()
 
-day3part1()
+day5part1()
